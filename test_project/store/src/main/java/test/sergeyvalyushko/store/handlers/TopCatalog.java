ec88232120dda94aa7652dbfdc4066f29f189bc2
Sergey Valyushko <sergeyvalyushko@coherentsolutions.com>
2020-09-02 20:09:38 +0300

    task5_commit5

--- test_project/store/src/main/java/test/sergeyvalyushko/store/handlers/TopCatalog.java
+++ test_project/store/src/main/java/test/sergeyvalyushko/store/handlers/TopCatalog.java
@@ -1,23 +1,18 @@
 package test.sergeyvalyushko.store.handlers;
 
-import org.xml.sax.SAXException;
-import test.sergeyvalyushko.store.Catalog;
 import test.sergeyvalyushko.store.Product;
 
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TopCatalog extends SortCatalog {
 
     public TopCatalog(AbstractHandler nextHandler) {
         super(nextHandler);
-        sortParameters.add("top");
-    }
-
-    @Override
-    public void handleRequest(String input, Catalog catalog) throws ParserConfigurationException, SAXException, IOException {
-        super.handleRequest(input, catalog);
+        List<String> sortParametersTop = new ArrayList<>();
+        sortParametersTop.add("top");
+        sortParameters = sortParametersTop;
     }
 
     @Override
