9c0fea93e4f75f5d7fb15c3b8bbad22c90717f32
Sergey Valyushko <sergeyvalyushko@coherentsolutions.com>
2020-09-08 16:45:28 +0300

    task5_commit5

--- test_project/store/src/main/java/test/sergeyvalyushko/store/handlers/TopCatalog.java
+++ test_project/store/src/main/java/test/sergeyvalyushko/store/handlers/TopCatalog.java
@@ -3,15 +3,14 @@ package test.sergeyvalyushko.store.handlers;
 import test.sergeyvalyushko.store.Product;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TopCatalog extends SortCatalog {
 
     public TopCatalog(AbstractHandler nextHandler) {
         super(nextHandler);
-        List<String> sortParametersTop = new ArrayList<>();
-        sortParametersTop.add("top");
+        List<String> sortParametersTop = Arrays.asList("top");
         sortParameters = sortParametersTop;
     }
 
